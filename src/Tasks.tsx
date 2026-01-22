11449198f63ebad40061aa3bb7bb71a5c214860c
unknown <mirin19771@gmail.com>
2026-01-22 18:39:54 +0700

    fix on edit task

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -1,71 +1,74 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Button from "./components/Button";
-import type { Task } from "./types";
+import type { TaskItemProps, TaskListProps } from "./types";
 
-interface TasksProps {
-  tasks: Task[];
-  onEdit: (task: Task) => void;
-  onDelete: (id: string) => void;
-}
-
-const Tasks: React.FC<TasksProps> = ({ tasks, onEdit, onDelete }) => {
-  const [isEdit, setIsEdit] = useState<boolean>(false);
+const TaskList = ({ tasks, onEdit, onDelete }: TaskListProps) => {
   return (
     <>
       {tasks.map((task) => {
-        const { id, text, done }: Task = task;
         return (
-          <>
-            <div className="flex gap-3 items-center">
-              <input
-                type="checkbox"
-                checked={done}
-                onChange={(e) => {
-                  onEdit({
-                    ...task,
-                    done: e.target.checked,
-                  });
-                }}
-              />
-
-              {isEdit ? (
-                <input
-                  type="text"
-                  value={text}
-                  onChange={(e) => {
-                    onEdit({
-                      ...task,
-                      text: e.target.value,
-                    });
-                  }}
-                />
-              ) : (
-                <p className="inline">{text}</p>
-              )}
-
-              <Button
-                variant="danger"
-                onClick={() => {
-                  onDelete(id);
-                }}
-              >
-                delete
-              </Button>
-
-              <Button
-                variant="primary"
-                onClick={() => {
-                  setIsEdit((prev) => !prev);
-                }}
-              >
-                {isEdit ? "save" : "edit"}
-              </Button>
-            </div>
-          </>
+          <Tasks
+            key={task.id}
+            task={task}
+            onEdit={onEdit}
+            onDelete={onDelete}
+          />
         );
       })}
     </>
   );
 };
 
-export default Tasks;
+export default TaskList;
+
+const Tasks = ({ task, onEdit, onDelete }: TaskItemProps) => {
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const { id, text, done } = task;
+  return (
+    <div className="flex gap-3 items-center">
+      <input
+        type="checkbox"
+        checked={done}
+        onChange={(e) => {
+          onEdit({
+            ...task,
+            done: e.target.checked,
+          });
+        }}
+      />
+
+      {isEdit ? (
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => {
+            onEdit({
+              ...task,
+              text: e.target.value,
+            });
+          }}
+        />
+      ) : (
+        <p className="inline">{text}</p>
+      )}
+
+      <Button
+        variant="danger"
+        onClick={() => {
+          onDelete(id);
+        }}
+      >
+        delete
+      </Button>
+
+      <Button
+        variant="primary"
+        onClick={() => {
+          setIsEdit((prev) => !prev);
+        }}
+      >
+        {isEdit ? "save" : "edit"}
+      </Button>
+    </div>
+  );
+};
--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,16 @@ export type Action =
   | { type: "add"; payload: Task }
   | { type: "edit"; payload: Task }
   | { type: "delete"; payload: { id: string } };
+
+type TaskCallbacks = {
+  onEdit: (task: Task) => void;
+  onDelete: (id: string) => void;
+};
+
+export interface TaskListProps extends TaskCallbacks {
+  tasks: Task[];
+}
+
+export interface TaskItemProps extends TaskCallbacks {
+  task: Task;
+}
